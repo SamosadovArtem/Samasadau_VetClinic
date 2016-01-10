6e9c439338d25105634e83db96ef819ac389375b
Artem <pizzaeueu@gmail.com>
2016-01-10 16:26:55 +0300

    First commit. Add database, some package, work with db, dependency injection (Ninject)

---
+++ VetClinic/VetClinic/VetClinic/Models/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinic.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Doctor> GetDoctors();
+        bool AddSchedule(Schedule instance);
+        //bool Update();
+        //bool Delete();
+    }
+}
---
+++ VetClinic/VetClinic/VetClinic/Models/SQLRepository/SQLRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace VetClinic.Models.SQLRepository
+{
+    public class SQLRepository:IRepository
+    {
+
+        VetDBDataContext db { get; set; }
+
+        public SQLRepository()
+        {
+            db = DependencyResolver.Current.GetService<VetDBDataContext>();
+        }
+        public IQueryable<Doctor> GetDoctors()
+        {
+            return db.Doctor;
+        }
+        public bool AddSchedule(Schedule instance)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
