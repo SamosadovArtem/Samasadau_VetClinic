a756b4500ccd9df36df4c7b1129ee96adbcb31f9
unknown <pizzaeueu@gmail.com>
2016-01-27 12:15:08 +0300

    Настроил доступ к админке по соответствующей роли, добавил страницу 404 и перенаправления

---
+++ VetClinic/VetClinic/Areas/Default/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VetClinic.Areas.Default.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Default/Error/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
